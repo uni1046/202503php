abaf69407d16f73c3cceedf2c241ab7630e60bc5
uni1046 <uniommmm@gmail.com>
2025-05-01 22:58:47 +0800

    initial by laravel 12

--- php/laravel/routes/web.php
+++ php/laravel/routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\TestMyController;
 use Illuminate\Support\Facades\Route;
-
-
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-//Route::get('/welcome',[TestsController::class,'index'])->name('tests.index');
-Route::get('/mypage',[TestMyController::class,'index'])->name('tests.mypage');
